4ad685f3a3504d7e0a3844d42532c7dab66d6140
aditya Kharade <adityakharade51@gmail.com>
2020-10-15 20:20:40 +0530

    Added : added functionality to find best rated cheapest hotel

--- src/com/hotelreservation/HotelReservationTest.java
+++ src/com/hotelreservation/HotelReservationTest.java
@@ -32,7 +32,6 @@ class HotelReservationTest {
 		hotel.add("Lakewood", 110, 90);
 		hotel.add("Bridgewood", 150, 50);
 		hotel.add("Ridgewood", 220, 150);
-		hotel.printHotels();
 		int result = hotel.size();
 		assertEquals(3, result);
 	}
@@ -47,7 +46,6 @@ class HotelReservationTest {
 		hotel.add("Lakewood", 110, 90);
 		hotel.add("Bridgewood", 150, 50);
 		hotel.add("Ridgewood", 220, 150);
-		hotel.printHotels();
 		List<String> result = new ArrayList<>();
 		try {
 			result = hotel.cheapestHotel("12Sep2020", "13Sep2020");
@@ -66,10 +64,9 @@ class HotelReservationTest {
 		hotel.add("Lakewood", 330, 120);
 		hotel.add("Bridgewood", 160, 60);
 		hotel.add("Ridgewood", 440, 180);
-		hotel.printHotels();
 		List<String> result = new ArrayList<>();
 		try {
-			result = hotel.cheapestHotel("08Sep2020", "11Sep2020");
+			result = hotel.cheapestHotel("10Sep2020", "11Sep2020");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
@@ -85,7 +82,6 @@ class HotelReservationTest {
 		hotel.add("Lakewood", 110, 90);
 		hotel.add("Bridgewood", 150, 50);
 		hotel.add("Ridgewood", 220, 150);
-		hotel.printHotels();
 		List<String> result = new ArrayList<>();
 		try {
 			result = hotel.cheapestHotel("11Sep2020", "12Sep2020");
@@ -105,9 +101,44 @@ class HotelReservationTest {
 		hotel.add("Lakewood", 110, 90, 3);
 		hotel.add("Bridgewood", 150, 50, 4);
 		hotel.add("Ridgewood", 220, 150, 5);
-		hotel.printHotels();
 		int result = hotel.size();
 		assertEquals(3, result);
+	}
+
+	/**
+	 * Usecase6 for finding cheapest best rated hotel
+	 */
+	@Test
+	public void givenHotelDatawithRatings_WhenAddedToMap_ShouldReturnBestRated() {
+		HotelReservation hotel = new HotelReservation();
+		hotel.add("Lakewood", 110, 90, 3);
+		hotel.add("Bridgewood", 150, 50, 4);
+		hotel.add("Ridgewood", 220, 150, 5);
+		String result = "";
+		try {
+			result = hotel.cheapestBestRated("11Sep2020", "12Sep2020");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		assertEquals("Bridgewood", result);
+	}
+
+	/**
+	 * Finding cheapest best rated hotel(handled for out of bound exception )
+	 */
+	@Test
+	public void givenNewDatawithRatings_WhenAddedToMap_ShouldReturnBestRated() {
+		HotelReservation hotel = new HotelReservation();
+		hotel.add("Lakewood", 200, 90, 3);
+		hotel.add("Bridgewood", 180, 50, 4);
+		hotel.add("Ridgewood", 150, 150, 5);
+		String result = "";
+		try {
+			result = hotel.cheapestBestRated("11Sep2020", "12Sep2020");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		assertEquals("Bridgewood", result);
 	}
 
 }
